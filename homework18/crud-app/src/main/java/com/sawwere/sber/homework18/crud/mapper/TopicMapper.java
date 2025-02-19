6bf5d164e46e45247605d8c2251e245e1d5ad8eb
sawwere <sawer_13@mail.ru>
2025-02-19 20:20:55 +0300

    Add rest controller

--- homework18/crud-app/src/main/java/com/sawwere/sber/homework18/crud/mapper/TopicMapper.java
+++ homework18/crud-app/src/main/java/com/sawwere/sber/homework18/crud/mapper/TopicMapper.java
@@ -2,11 +2,19 @@ package com.sawwere.sber.homework18.crud.mapper;
 
 import com.sawwere.sber.homework18.crud.domain.Topic;
 import com.sawwere.sber.homework18.crud.dto.TopicCreationDto;
-import org.mapstruct.Mapper;
-import org.mapstruct.MappingConstants;
-import org.mapstruct.ReportingPolicy;
+import com.sawwere.sber.homework18.crud.dto.TopicDto;
+import org.mapstruct.*;
 
 @Mapper(unmappedTargetPolicy = ReportingPolicy.IGNORE, componentModel = MappingConstants.ComponentModel.SPRING)
 public interface TopicMapper {
     Topic mapToEntity(TopicCreationDto topicCreationDto);
+
+    Topic mapToEntity(TopicDto topicDto);
+
+    TopicDto mapToDto(Topic topic);
+
+    @AfterMapping
+    default void linkReplies(@MappingTarget Topic topic) {
+        topic.getReplies().forEach(reply -> reply.setTopic(topic));
+    }
 }
