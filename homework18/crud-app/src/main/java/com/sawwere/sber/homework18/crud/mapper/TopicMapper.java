a404decb3c929e02855044d48fa3ec911248d702
sawwere <81819674+sawwere@users.noreply.github.com>
2025-02-19 19:05:14 +0300

    Add homework21 (#18)

--- homework18/crud-app/src/main/java/com/sawwere/sber/homework18/crud/mapper/TopicMapper.java
+++ homework18/crud-app/src/main/java/com/sawwere/sber/homework18/crud/mapper/TopicMapper.java
@@ -2,18 +2,11 @@ package com.sawwere.sber.homework18.crud.mapper;
 
 import com.sawwere.sber.homework18.crud.domain.Topic;
 import com.sawwere.sber.homework18.crud.dto.TopicCreationDto;
-import org.springframework.stereotype.Component;
+import org.mapstruct.Mapper;
+import org.mapstruct.MappingConstants;
+import org.mapstruct.ReportingPolicy;
 
-import java.util.ArrayList;
-
-@Component
-public class TopicMapper {
-    public Topic mapToEntity(TopicCreationDto dto) {
-        return Topic.builder()
-                .title(dto.getTitle())
-                .text(dto.getText())
-                .author(dto.getAuthor())
-                .replies(new ArrayList<>())
-                .build();
-    }
+@Mapper(unmappedTargetPolicy = ReportingPolicy.IGNORE, componentModel = MappingConstants.ComponentModel.SPRING)
+public interface TopicMapper {
+    Topic mapToEntity(TopicCreationDto topicCreationDto);
 }
